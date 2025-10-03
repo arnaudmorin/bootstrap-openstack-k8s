15cd08b09eef479c9eb35a94ad4c1618cbb4f261
Arnaud M <arnaud.choupinette@mailops.fr>
2025-10-03 21:50:09 +0200

    Use consistant naming for network interfaces
    Signed-off-by: Arnaud M <arnaud.choupinette@mailops.fr>

--- tofu/main.tf
+++ tofu/main.tf
@@ -11,6 +11,11 @@ provider "openstack" {
 
 }
 
+
+#
+# Variables
+#
+
 variable "openstack_version" {
   type        = string
   description = "Openstack version to deploy the cluster, it used to reference branch of bootstrap-openstack-k8s openstack-docker repos and as the tag of the os services images to pull"
@@ -19,14 +24,14 @@ variable "openstack_version" {
 
 variable "compute_count" {
   description = "Number of compute nodes"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 variable "network_count" {
   description = "Number of network node"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 resource "random_password" "password" {
@@ -38,15 +43,21 @@ data "openstack_networking_network_v2" "public" {
   name = "public"
 }
 
+#
+# Ports
+#
+# NOTE(arnaud) we need to create ports in a separate way to have access to their mac_address
+
 resource "openstack_networking_port_v2" "public_port" {
-  count = var.compute_count + var.network_count
-  name = "${terraform.workspace}-public-port-${count.index}"
-  network_id = data.openstack_networking_network_v2.public.id
+  count          = var.compute_count + var.network_count
+  name           = "${terraform.workspace}-public-port-${count.index}"
+  network_id     = data.openstack_networking_network_v2.public.id
   admin_state_up = "true"
+  tags           = ["role=management"]
 }
 
 resource "openstack_networking_network_v2" "octavia_mgmt" {
-  name = "${terraform.workspace}-octavia-mgmt"
+  name                  = "${terraform.workspace}-octavia-mgmt"
   port_security_enabled = false
   value_specs = {
     "provider:network_type" = "vrack"
@@ -54,11 +65,16 @@ resource "openstack_networking_network_v2" "octavia_mgmt" {
 }
 
 resource "openstack_networking_port_v2" "octavia_port" {
-  count = var.compute_count + var.network_count + 1
-  name = "${terraform.workspace}-octavia-port-${count.index}"
-  network_id = openstack_networking_network_v2.octavia_mgmt.id
+  count          = var.compute_count + var.network_count + 1
+  name           = "${terraform.workspace}-octavia-port-${count.index}"
+  network_id     = openstack_networking_network_v2.octavia_mgmt.id
   admin_state_up = "true"
 }
+
+
+#
+# Instances
+#
 
 resource "openstack_compute_keypair_v2" "zob" {
   name       = "${terraform.workspace}-zob"
@@ -76,6 +92,7 @@ resource "openstack_compute_instance_v2" "k8s" {
       path_module = path.module,
       password    = random_password.password.result
       os_version  = var.openstack_version
+      mac_octavia = openstack_networking_port_v2.octavia_port[count.index].mac_address
     }
   )
   key_pair = openstack_compute_keypair_v2.zob.name
@@ -84,10 +101,12 @@ resource "openstack_compute_instance_v2" "k8s" {
   }
 }
 
+# NOTE(arnaud) we need to attach ports separately because they do not have
+# any subnet
 resource "openstack_compute_interface_attach_v2" "attach_octavia_k8s" {
-  count = 1
+  count       = 1
   instance_id = openstack_compute_instance_v2.k8s[count.index].id
-  port_id = openstack_networking_port_v2.octavia_port[count.index].id
+  port_id     = openstack_networking_port_v2.octavia_port[count.index].id
 }
 
 resource "openstack_compute_instance_v2" "computes" {
@@ -103,6 +122,8 @@ resource "openstack_compute_instance_v2" "computes" {
       k8s_ip      = openstack_compute_instance_v2.k8s[0].access_ip_v4
       os_version  = var.openstack_version
       hostname    = "compute-${count.index}"
+      mac_public  = openstack_networking_port_v2.public_port[count.index].mac_address
+      mac_octavia = openstack_networking_port_v2.octavia_port[count.index + 1].mac_address
     }
   )
   key_pair = openstack_compute_keypair_v2.zob.name
@@ -112,15 +133,15 @@ resource "openstack_compute_instance_v2" "computes" {
 }
 
 resource "openstack_compute_interface_attach_v2" "attach_public_computes" {
-  count = var.compute_count
+  count       = var.compute_count
   instance_id = openstack_compute_instance_v2.computes[count.index].id
-  port_id = openstack_networking_port_v2.public_port[count.index].id
+  port_id     = openstack_networking_port_v2.public_port[count.index].id
 }
 
 resource "openstack_compute_interface_attach_v2" "attach_octavia_computes" {
-  count = var.compute_count
+  count       = var.compute_count
   instance_id = openstack_compute_instance_v2.computes[count.index].id
-  port_id = openstack_networking_port_v2.octavia_port[count.index + 1].id
+  port_id     = openstack_networking_port_v2.octavia_port[count.index + 1].id
 }
 
 resource "openstack_compute_instance_v2" "networks" {
@@ -136,6 +157,8 @@ resource "openstack_compute_instance_v2" "networks" {
       k8s_ip      = openstack_compute_instance_v2.k8s[0].access_ip_v4
       os_version  = var.openstack_version
       hostname    = "network-${count.index}"
+      mac_public  = openstack_networking_port_v2.public_port[count.index + var.compute_count].mac_address
+      mac_octavia = openstack_networking_port_v2.octavia_port[count.index + var.compute_count + 1].mac_address
     }
   )
   key_pair = openstack_compute_keypair_v2.zob.name
@@ -145,16 +168,21 @@ resource "openstack_compute_instance_v2" "networks" {
 }
 
 resource "openstack_compute_interface_attach_v2" "attach_public_networks" {
-  count = var.network_count
+  count       = var.network_count
   instance_id = openstack_compute_instance_v2.networks[count.index].id
-  port_id = openstack_networking_port_v2.public_port[count.index + var.compute_count].id
+  port_id     = openstack_networking_port_v2.public_port[count.index + var.compute_count].id
 }
 
 resource "openstack_compute_interface_attach_v2" "attach_octavia_networks" {
-  count = var.network_count
+  count       = var.network_count
   instance_id = openstack_compute_instance_v2.networks[count.index].id
-  port_id = openstack_networking_port_v2.octavia_port[count.index + 1 + var.compute_count].id
+  port_id     = openstack_networking_port_v2.octavia_port[count.index + 1 + var.compute_count].id
 }
+
+
+#
+# Outputs
+#
 
 output "ssh_commands" {
   value = merge(
